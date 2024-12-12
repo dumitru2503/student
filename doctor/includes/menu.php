3a43d583e776b23820bc4b8d8bcbd7e0f31ac36f
dumitru2503 <dumitru2503@gmail.com>
2024-12-13 00:09:36 +0200

    Resolved confilcts

--- doctor/includes/menu.php
+++ doctor/includes/menu.php
@@ -15,20 +15,14 @@
             </td>
           </tr>
           <tr>
-<<<<<<< HEAD
-          <td colspan="2">
-  <a href="../logout.php">
-    <button class="logout-btn btn-primary-soft btn">
-       Log Out <i class="fa-solid fa-right-from-bracket" style="margin-right: 8px; margin-left: 6px"></i>
-    </button>
-  </a>
-</td>
-
-=======
             <td colspan="2">
-              <a href="../logout.php"><input type="button" value="Log Out" class="logout-btn btn-primary-soft btn"></a>
+              <a href="../logout.php">
+                <button class="logout-btn btn-primary-soft btn">
+                  Log Out <i class="fa-solid fa-right-from-bracket" style="margin-right: 8px; margin-left: 6px"></i>
+                </button>
+              </a>
             </td>
->>>>>>> 1e60ad79324fcfa9cf015c2d7c151f7940760f49
+
           </tr>
         </table>
       </td>
@@ -37,13 +31,8 @@
       <td class="menu-btn menu-icon-home">
         <a href="index.php">
           <div>
-<<<<<<< HEAD
-            <p class="menu-text"><i class="fa-solid fa-house" style="margin-right: 12px; padding: 10px; "></i>Dashboard</p>
-=======
-            <p class="menu-text"><i class="fa-solid fa-house"
-                style="margin-right: 12px;border: 2px solid #ffffff; border-radius: 50%; padding: 10px; color: #ffffff;"></i>Dashboard
+            <p class="menu-text"><i class="fa-solid fa-house" style="margin-right: 12px; padding: 10px; "></i>Dashboard
             </p>
->>>>>>> 1e60ad79324fcfa9cf015c2d7c151f7940760f49
           </div>
         </a>
       </td>
@@ -52,13 +41,8 @@
       <td class="menu-btn menu-icon-appoinment">
         <a href="appointment.php">
           <div>
-<<<<<<< HEAD
-            <p class="menu-text"><i class="fa-solid fa-calendar-days" style="margin-right: 12px; padding: 10px; "></i>Appointments</p>
-=======
             <p class="menu-text"><i class="fa-solid fa-calendar-days"
-                style="margin-right: 12px;border: 2px solid #ffffff; border-radius: 50%; padding: 10px; color: #ffffff;"></i>Appointments
-            </p>
->>>>>>> 1e60ad79324fcfa9cf015c2d7c151f7940760f49
+                style="margin-right: 12px; padding: 10px; "></i>Appointments</p>
           </div>
         </a>
       </td>
@@ -67,13 +51,8 @@
       <td class="menu-btn menu-icon-appoinment">
         <a href="patient.php">
           <div>
-<<<<<<< HEAD
-            <p class="menu-text"><i class="fa-solid fa-user" style="margin-right: 12px; padding: 10px; "></i>Patients</p>
-=======
-            <p class="menu-text"><i class="fa-solid fa-user"
-                style="margin-right: 12px;border: 2px solid #ffffff; border-radius: 50%; padding: 10px; color: #ffffff;"></i>Patients
+            <p class="menu-text"><i class="fa-solid fa-user" style="margin-right: 12px; padding: 10px; "></i>Patients
             </p>
->>>>>>> 1e60ad79324fcfa9cf015c2d7c151f7940760f49
           </div>
         </a>
       </td>
@@ -92,13 +71,7 @@
       <td class="menu-btn menu-icon-schedule">
         <a href="schedule.php">
           <div>
-<<<<<<< HEAD
             <p class="menu-text"><i class="fa-solid fa-list" style="margin-right: 12px; padding: 10px; "></i>Orar</p>
-=======
-            <p class="menu-text"><i class="fa-solid fa-list"
-                style="margin-right: 12px;border: 2px solid #ffffff; border-radius: 50%; padding: 10px; color: #ffffff; "></i>Orar
-            </p>
->>>>>>> 1e60ad79324fcfa9cf015c2d7c151f7940760f49
           </div>
         </a>
       </td>
@@ -107,14 +80,8 @@
       <td class="menu-btn menu-icon-settings">
         <a href="settings.php">
           <div style="margin-top: 50px;">
-<<<<<<< HEAD
             <p class="menu-text"><i class="fas fa-cog" style="margin-right: 12px; padding: 10px; "></i>
-            Settings</p>
-=======
-            <p class="menu-text"><i class="fas fa-cog"
-                style="margin-right: 12px;border: 2px solid #ffffff; border-radius: 50%; padding: 10px; color: #ffffff;"></i>
               Settings</p>
->>>>>>> 1e60ad79324fcfa9cf015c2d7c151f7940760f49
           </div>
         </a>
       </td>
