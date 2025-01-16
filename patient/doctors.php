d132135c567de85cc06a0313a72e67c089ff1866
dumitru2503 <dumitru2503@gmail.com>
2025-01-16 02:08:10 +0200

    add doctors view

--- patient/doctors.php
+++ patient/doctors.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" href="../css/main.css">
     <link rel="stylesheet" href="../css/admin.css">
 
-    <title>Doctors</title>
+    <title>Medici</title>
     <style>
         .popup {
             animation: transitionIn-Y-bottom 0.5s;
@@ -34,6 +34,8 @@
         header("location: ../login.php");
     }
 
+
+    //import database
     include("../connection.php");
 
     date_default_timezone_set('Europe/Bucharest');
@@ -48,6 +50,7 @@
     ?>
     <div class="container">
         <?php require_once './includes/menu.php' ?>
+
         <div class="dash-body">
             <table border="0" width="100%" style=" border-spacing: 0;margin:0;padding:0;margin-top:25px; ">
                 <tr class="header">
@@ -84,25 +87,126 @@
 
 
                 <tr>
-                    <td colspan="4" style="padding-top:10px;">
-                        <p class="heading-main12" style="margin-left: 45px;font-size:18px;color:rgb(49, 49, 49)">All
-                            Doctors
+                    <td colspan="4" style="padding-top:10px;width: 100%;">
+                        <p class="heading-main12" style="margin-left: 45px;font-size:18px;color:rgb(49, 49, 49)">
+                            Medici
+                        </p>
+                        <p class="heading-main12" style="margin-left: 45px;font-size:22px;color:rgb(49, 49, 49)">
+                            <?php echo strlen($search_term) != 0 ? "'$search_term'" : ""; ?>
+                        </p>
                     </td>
 
                 </tr>
+
+
 
                 <tr>
                     <td colspan="4">
                         <center>
-                            <p>
-                                TODO: adauga setari
-                            </p>
-                        </center>
-                    </td>
-                </tr>
-            </table>
-        </div>
+                            <div class="abc scroll">
+                                <table width="100%" class="sub-table scrolldown" border="0"
+                                    style="padding: 50px;border:none">
+
+                                    <tbody>
+
+                                        <?php
+
+                                        $type_doctor = "d";
+                                        $stmt = $database->prepare("SELECT * FROM users WHERE name LIKE ? and type = ?");
+                                        $search_pattern = "%" . $search_term . "%";
+                                        $stmt->bind_param("ss", $search_pattern, $type_doctor);
+                                        $stmt->execute();
+                                        $result = $stmt->get_result();
+
+
+
+                                        if ($result->num_rows == 0) {
+                                            ?>
+                                            <tr>
+                                                <td colspan="4">
+                                                    <br><br><br><br>
+                                                    <center>
+                                                        <img src="../img/notfound.svg" width="25%">
+
+                                                        <br>
+                                                        <p class="heading-main12"
+                                                            style="margin-left: 45px;font-size:20px;color:rgb(49, 49, 49)">
+                                                            Nu am putut gasi nimic !</p>
+                                                    </center>
+                                                    <br><br><br><br>
+                                                </td>
+                                            </tr>
+
+                                            <?php
+
+                                        } else {
+
+                                            for ($x = 0; $x < ($result->num_rows); $x++) { ?>
+                                                <tr>
+
+                                                <?php
+                                                for ($q = 0; $q < 3; $q++) {
+                                                    $row = $result->fetch_assoc();
+                                                    if (!isset($row)) {
+                                                        break;
+                                                    }
+                                                    ;
+                                                    $doctor_id = $row["id"];
+                                                    $doctor_name = $row["name"];
+                                                    // $service_image = $row["image"];
+
+                                                    if ($doctor_id == "") {
+                                                        break;
+                                                    }
+
+
+                                                    $stmt = $database->prepare("SELECT s.name AS service_name 
+                                                                            FROM doctor_services AS ds
+                                                                            INNER JOIN services AS s ON ds.service_id = s.id 
+                                                                            WHERE doctor_id = ?");
+
+                                                    $stmt->bind_param("i", $doctor_id); // Assuming doctor_id is an integer
+                                                    $stmt->execute();
+                                                    $services = $stmt->get_result();
+
+                                                    ?>
+                                                    <td style="width: 25%;">
+                                                        <div class="dashboard-items search-items">
+                                                            <div style="width:100%">
+                                                                <a href="#?id=<?php echo $doctor_id; ?>">
+                                                                    <div class="service-container">
+                                                                        <img
+                                                                            src="../img/doctor.png">
+                                                                        <br>
+                                                                        <div class="h3-search">
+                                                                            <?php echo substr($doctor_name, 0, 21); ?>
+                                                                        </div>
+                                                                        
+                                                                    </div>
+                                                                </a>
+                                                            </div>
+                                                        </div>
+                                                    </td>
+                                                    
+
+                    
+                                                <?php } ?>
+                                                </tr>
+                                            <?php } ?>
+                                        <?php } ?>
+        </tbody>
+        </table>
     </div>
+    </center>
+    </td>
+    </tr>
+
+
+
+    </table>
+    </div>
+    </div>
+
     </div>
 
 </body>
